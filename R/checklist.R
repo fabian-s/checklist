a6ebb0a05e2c45c5e85e75e95a8686b2d4368541
fabian-s <fabian.scheipl@gmail.com>
2021-04-20 14:32:47 +0200

    ..

--- R/checklist.R
+++ R/checklist.R
@@ -8,6 +8,21 @@
 #' checklist::checklist(system.file("examples", package = "checklist"))
 #' @export
 checklist <- function(path = ".") {
+  run_checks(path)
+  
+  message("Ok, so far so good. If anything has come up above, please look into it & get it fixed.")
+  message("(But some of the lints and alerts above may also be false positives --")
+  message("use your judgement, since you're using this package you must have excellent taste anyway...)")
+  message("The easiest way to fix the formatting is to run 'styler::style_dir' or 'styler::style_file'")
+  message("#---------------------------------------------------------------#")
+  
+  nag_user()
+  
+  message("Ok, please fix everything that came up and then run me again to make sure. Bye!\n")
+  invisible(NULL)
+}
+
+run_checks <- function(path) {
   if (!file.exists(path)) stop("Can't find directory or file at ", path, ".")
   message("Starting checks.")
   try(check_extensions(path))
@@ -19,13 +34,11 @@ checklist <- function(path = ".") {
   message("#---------------------------------------------------------------#")
   try(check_usage(path))
   message("#---------------------------------------------------------------#")
-  message("Ok, so far so good. If anything has come up above, please look into it & get it fixed.")
-  message("(But some of the lints and alerts above may also be false positives --")
-  message("use your judgement, since you're using this package you must have excellent taste anyway...)")
-  message("The easiest way to fix the formatting is to run 'styler::style_dir' or 'styler::style_file'")
-  message("#---------------------------------------------------------------#")
+  invisible(NULL)
+}
+
+nag_user <- function() {
   message("Now for some non-automated checks you need to do yourself:")
-  
   defensive <- readline(
     paste0("Think about defensive programming ----\n",
            "Did you include argument checks (using {checkmate}) in all your functions?\n",
@@ -35,10 +48,10 @@ checklist <- function(path = ".") {
   if (defensive == "n") message("Uh oh! You're asking for trouble. Please do that now.")
   smell <- readline(
     paste0("Think about code smells ----\n",
-    "Did you try to avoid conditional complexity?\n", 
-    "(avoiding 'else', homogenizing inputs, using early exits, etc..)\n",
-    "You never did 'copy-paste-modify' anywhere, right? Keep it DRY, don't get WET!\n",
-    "(y/n)"))
+           "Did you try to avoid conditional complexity?\n", 
+           "(avoiding 'else', homogenizing inputs, using early exits, etc..)\n",
+           "You never did 'copy-paste-modify' anywhere, right? Keep it DRY, don't get WET!\n",
+           "(y/n)"))
   if (smell == "n") message("Not good! Go back and make it better, please...")
   names <- readline(
     paste0("Re-check your variable and function names --\n",
@@ -47,6 +60,5 @@ checklist <- function(path = ".") {
            "(y/n)"))
   if (names == "n") message("Well, do better. Try to make your code easy to understand.")
   message("#---------------------------------------------------------------#")
-  message("Ok, please fix everything that came up and then run me again to make sure. Bye!\n")
   invisible(NULL)
 }
